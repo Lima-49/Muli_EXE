8dd09fc7b91c698e980e5b1c6a47c2be3da25398
Lima-49 <vitorlima.soares@hotmail.com>
2022-08-17 13:44:26 -0300

    Atualização

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
-from importlib.metadata import packages_distributions
 from setuptools import setup
 
 setup(name='multi-exe-maker',
-    version='0.1',
+    version='0.2',
     long_description=''.join(open('README.md', encoding='utf-8').readlines()),
     long_description_content_type='text/markdown',
     author="Vitor Augusto de Lima Soares",
